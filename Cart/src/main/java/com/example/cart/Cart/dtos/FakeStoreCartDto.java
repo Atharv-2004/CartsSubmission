cbcf77ebccb7fbe9d8aacc6f42a1da167498cb4c
Atharv-2004 <140976954+Atharv-2004@users.noreply.github.com>
2024-03-12 17:45:26 +0530

    Add files via upload

---
+++ Cart/src/main/java/com/example/cart/Cart/dtos/FakeStoreCartDto.java
@@ -0,0 +1,11 @@
+package com.example.cart.Cart.dtos;
+
+import com.example.cart.Cart.models.carts;
+
+public class FakeStoreCartDto {
+    /* id:1,
+    userId:1,
+    date:2020-10-10,
+    products:[{productId:1,quantity:2},{productId:2,quantity:3}]*/
+     carts cart;
+}
---
+++ Cart/src/main/java/com/example/cart/Cart/models/carts.java
@@ -0,0 +1,8 @@
+package com.example.cart.Cart.models;
+import java.util.*;
+public class carts {
+    ArrayList<cart> carts;
+    carts(ArrayList<cart> carts){
+        this.carts = carts;
+    }
+}
